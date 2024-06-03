95dfa9ebc2cd9ed54c99011babe5dc6e2e129e8d
Kristi Norris <kristi.n.norris777@gmail.com>
2024-06-03 11:22:50 -0700

    Added tables and default insterted data to database

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,25 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO topping_tiers (topping_tier, tier_description, tier_price_modifyer) 
+VALUES (0, 'Essentials', 0), (1, 'Need to haves', 1.25), (2, 'Premium', 2.25);
+
+INSERT INTO toppings (topping, topping_tier)
+VALUES ('Pepperoni', 1), ('Basil', 1), ('Italian Sausage', 2), ('Mushrooms', 0), ('Onions', 0), ('Green Peppers', 0), ('Black Olives', 0), ('Green Olives', 0), 
+('Tomato slices', 1), ('Jalapenos', 1), ('Bacon', 2);
+
+INSERT INTO pizza_sizes (pizza_size, pizza_base_price) VALUES ('Small', 6.75), ('Medium', 9.75), ('Large', 12.25);
+
+INSERT INTO crusts (crust_type, type_price_modifyer) VALUES ('Regular', 0), ('Deep Dish', 4), ('Thin', 0), ('Cauliflower', 3.5);
+
+INSERT INTO specialty_pizzas  (pizza_name, pizza_size, crust_type) VALUES ('Margharita', 'Medium', 'Regular'), ('Pepperoni', 'Large', 'Regular'),
+('Supreme', 'Large', 'Thin');
+
+INSERT INTO specialty_toppings (pizza_id, topping) VALUES (1, 'Basil'), (1, 'Tomato slices'), (2, 'Pepperoni'), (3, 'Green Peppers'), (3, 'Black Olives'), 
+	(3, 'Bacon'), (3, 'Mushrooms'), (3, 'Italian Sausage'), (3, 'Onions');
+
+INSERT INTO orders (order_total, order_status, order_type, delivery_address) 
+VALUES (12.25, 'Recieved', 'Pick-up', 'In-Store');
+
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,16 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS topping_tiers;
+DROP TABLE IF EXISTS toppings;
+DROP TABLE IF EXISTS pizza_sizes;
+DROP TABLE IF EXISTS crusts;
+DROP TABLE IF EXISTS specialty_pizzas;
+DROP TABLE IF EXISTS specialty_toppings;
+DROP TABLE IF EXISTS orders;
+DROP TYPE IF EXISTS pizza_sizes_t;
+DROP TYPE IF EXISTS order_status_t;
+DROP TYPE IF EXISTS order_type_t;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +19,55 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+CREATE TABLE topping_tiers (
+	topping_tier int NOT NULL UNIQUE DEFAULT 0,
+	tier_description varchar(50),
+	tier_price_modifyer NUMERIC(5, 2),
+	CONSTRAINT PK_topping_tier PRIMARY KEY (topping_tier)
+);
+CREATE TABLE toppings (
+	topping varchar(50) NOT NULL UNIQUE,
+	topping_tier int NOT NULL DEFAULT 0,
+	topping_available BOOL DEFAULT TRUE,
+	CONSTRAINT PK_topping PRIMARY KEY (topping),	
+	CONSTRAINT FK_topping_tier FOREIGN KEY (topping_tier) REFERENCES topping_tiers (topping_tier)
+);
+CREATE TYPE pizza_sizes_t AS ENUM ('Small', 'Medium', 'Large');
+CREATE TABLE pizza_sizes (
+	pizza_size pizza_sizes_t,
+	pizza_base_price NUMERIC(6, 2),
+	CONSTRAINT PK_pizza_size PRIMARY KEY (pizza_size)
+);
+CREATE TABLE crusts (
+	crust_type varchar(50) NOT NULL UNIQUE DEFAULT 'Regular',
+	type_price_modifyer NUMERIC(3, 2),
+	CONSTRAINT PK_crust_type PRIMARY KEY (crust_type)
+);
+CREATE TABLE specialty_pizzas (
+	pizza_id SERIAL,
+	pizza_name varchar(50) NOT NULL,
+	pizza_size varchar(50),
+	crust_type varchar(50),
+	specialty_toippings varchar(50),
+	CONSTRAINT PK_pizza_id PRIMARY KEY (pizza_id)	
+);
+CREATE TABLE specialty_toppings (
+	pizza_id int,
+	topping varchar(50),
+	CONSTRAINT PK_pizza_defaults PRIMARY KEY (pizza_id, topping),
+	CONSTRAINT FK_pizza_id FOREIGN KEY(pizza_id) REFERENCES specialty_pizzas (pizza_id),
+	CONSTRAINT FK_pizza_topping FOREIGN KEY(topping) REFERENCES toppings (topping)
+);
+CREATE TYPE order_type_t AS ENUM ('Pick-up', 'Delivery');
+CREATE TYPE order_status_t AS ENUM ('Recieved', 'Ready', 'Out for Delivery', 'Completed', 'Canceled');
+CREATE TABLE orders (
+	order_id SERIAL,
+	order_total NUMERIC(6, 2),
+	order_status order_status_t DEFAULT 'Recieved',
+	order_type order_type_t DEFAULT 'Pick-up',
+	delivery_address varchar(50) DEFAULT 'In-store',
+	CONSTRAINT PK_order_id PRIMARY KEY (order_id)
+);
+
 
 COMMIT TRANSACTION;
