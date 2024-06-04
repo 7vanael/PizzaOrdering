3fe11a5dbdb419e56af061347a0a220d696c3290
Lauren Gates <mononoke813@gmail.com>
2024-06-04 16:00:56 -0500

    did crusts

--- vue/src/components/CustomPizzaMenu.vue
+++ vue/src/components/CustomPizzaMenu.vue
@@ -32,8 +32,8 @@
         <ul class="list-group list-group">
           <li class="list-group-item size-crust">Choose Size and Crust</li>
           <li class="list-group-item">
-            <div class="card card-radio my-20" style="width: 100%;">
-              <ul class="list-group">
+            <!-- <div class="card card-radio my-20" style="width: 100%;"> -->
+              <!-- <ul class="list-group">
                 <li class="list-group-item pg-1-card-header">Regular</li>
                 <li class="list-group-item">Garlic seasoned crust with a rich, buttery taste.
                 </li>
@@ -55,11 +55,11 @@
                   </label>
                   Large (14")
                 </li>
-              </ul>
-            </div>
+              </ul> -->
+            <!-- </div>
             <br>
-            <div class="card card-radio" style="width: 100%;">
-              <ul class="list-group">
+            <div class="card card-radio" style="width: 100%;"> -->
+              <!-- <ul class="list-group">
                 <li class="list-group-item pg-1-card-header">Deep Dish</li>
                 <li class="list-group-item">Chicago-style deep dish crust with a buttery, flaky exterior.
                 </li>
@@ -81,11 +81,11 @@
                   </label>
                   Large (14")
                 </li>
-              </ul>
-            </div>
+              </ul> -->
+            <!-- </div>
             <br>
-            <div class="card card-radio" style="width: 100%;">
-              <ul class="list-group">
+            <div class="card card-radio" style="width: 100%;"> -->
+              <!-- <ul class="list-group">
                 <li class="list-group-item pg-1-card-header">Thin</li>
                 <li class="list-group-item">Thin enough for optimum crispy to crunchy ratio.
                 </li>
@@ -107,11 +107,11 @@
                   </label>
                   Large (16")
                 </li>
-              </ul>
-            </div>
-            <br>
+              </ul> -->
+            <!-- </div>
+            <br> -->
             <div class="card card-radio" style="width: 100%;">
-              <ul class="list-group">
+              <!-- <ul class="list-group">
                 <li class="list-group-item pg-1-card-header">Cauliflower</li>
                 <li class="list-group-item">Gluten-free cauliflower crust with a buttery, flaky exterior.
                 </li>
@@ -133,30 +133,43 @@
                   </label>
                   Large (16")
                 </li>
-              </ul>
-            </div>
+              </ul> -->
+
+
+          <li v-for="crust in crustTypes" v-bind:key="crust.name" class="list-group-item">
+            <label>
+              <input type="radio" name="pizza-crust-type" value={{crust.name}}>
+            </label> {{ crust.name }} 
+              <li v-for="crust in crustSizes" v-bind:key="crust.name" class="list-group-item">
+                <label>
+                  <input type="radio" name="pizza-crust-size" value={{crust.name}}>
+                </label> {{ crust.name }}
+              </li>
           </li>
-          <li class="list-group-item">
-          </li>
-        </ul>
-        <div class="card-footer d-flex justify-content-end">
-          <button class="next-step" id="nextButton">Next Step ></button>
-          <a href="#pills-profile" data-toggle="tab"></a>
-        </div>
+
+      </div>
+      
+      </li>
+      </ul>
+
+      <div class="card-footer d-flex justify-content-end">
+        <button class="next-step" id="nextButton">Next Step ></button>
+        <a href="#pills-profile" data-toggle="tab"></a>
       </div>
     </div>
-    <!-- Cheese and Sauce -->
-    <div class="tab-pane" id="pills-sauce-cheese" role="tabpanel" aria-labelledby="pills-sauce-cheese-tab">
-      <div class="card" style="width: 100%;">
-        <ul class="list-group list-group">
-          <li class="list-group-item cheese-sauce">Choose Sauce and Cheese</li>
-          <li class="list-group-item">
-            <!--Sauce-->
-            <div class="card card-radio" style="width: 100%;">
-              <ul class="list-group list-group">
-                <li class="list-group-item pg-2-card-header">Pick Your Sauce</li>
-                
-                <!-- <li class="list-group-item">
+  </div>
+  <!-- Cheese and Sauce -->
+  <div class="tab-pane" id="pills-sauce-cheese" role="tabpanel" aria-labelledby="pills-sauce-cheese-tab">
+    <div class="card" style="width: 100%;">
+      <ul class="list-group list-group">
+        <li class="list-group-item cheese-sauce">Choose Sauce and Cheese</li>
+        <li class="list-group-item">
+          <!--Sauce-->
+          <div class="card card-radio" style="width: 100%;">
+            <ul class="list-group list-group">
+              <li class="list-group-item pg-2-card-header">Pick Your Sauce</li>
+
+              <!-- <li class="list-group-item">
                   <label>
                     <input type="checkbox" name="pizza-sauce" value="tomato">
                   </label> Tomato
@@ -177,30 +190,30 @@
                   </label> BBQ
                 </li> -->
 
-                <li v-for="sauce in sauces" v-bind:key="sauce.name"  class="list-group-item">
-                  <label>
-                    <input type="radio" name="pizza-sauce" value={{sauce.name}}>
-                  </label> {{sauce.name}}
-                </li>
-                
-                <li class="list-group-item">How Much?</li>
-                <li class="list-group-item">
-                  <label>
-                    <select id="sauce-amount" name="sauce">
-                      <option value="light">Light</option>
-                      <option value="normal">Normal</option>
-                      <option value="extra">Extra</option>
-                    </select>
-                  </label>
-                </li>
-              </ul>
-            </div>
-            <br />
-            <!--Cheese-->
-            <div class="card card-radio my-20" style="width: 100%;">
-              <ul class="list-group list-group">
-                <li class="list-group-item pg-2-card-header">Pick Your Cheese</li>
-                <!-- <li class="list-group-item">
+              <li v-for="sauce in sauces" v-bind:key="sauce.name" class="list-group-item">
+                <label>
+                  <input type="radio" name="pizza-sauce" value={{sauce.name}}>
+                </label> {{ sauce.name }}
+              </li>
+
+              <li class="list-group-item">How Much?</li>
+              <li class="list-group-item">
+                <label>
+                  <select id="sauce-amount" name="sauce">
+                    <option value="light">Light</option>
+                    <option value="normal">Normal</option>
+                    <option value="extra">Extra</option>
+                  </select>
+                </label>
+              </li>
+            </ul>
+          </div>
+          <br />
+          <!--Cheese-->
+          <div class="card card-radio my-20" style="width: 100%;">
+            <ul class="list-group list-group">
+              <li class="list-group-item pg-2-card-header">Pick Your Cheese</li>
+              <!-- <li class="list-group-item">
                   <label>
                     <input type="checkbox" name="pizza-cheese" value="house-blend">
                   </label> House Blend
@@ -210,45 +223,45 @@
                     <input type="checkbox" name="pizza-cheese" value="mozzarella">
                   </label> Fresh Mozzarella
                 </li> -->
-                <li v-for="cheese in cheeseToppings" v-bind:key="cheese.name" class="list-group-item">
-                  <label>
-                    <input type="radio" name="pizza-cheese" value={{cheese.name}}>
-                  </label> {{ cheese.name }}
+              <li v-for="cheese in cheeseToppings" v-bind:key="cheese.name" class="list-group-item">
+                <label>
+                  <input type="radio" name="pizza-cheese" value={{cheese.name}}>
+                </label> {{ cheese.name }}
 
-                </li>
-                <li class="list-group-item">How Much?</li>
-                <li class="list-group-item">
-                  <label>
-                    <select id="cheese-amount" name="cheese">
-                      <option value="light">Light</option>
-                      <option value="normal">Normal</option>
-                      <option value="extra">Extra</option>
-                      <option value="double">Double</option>
-                    </select>
-                  </label>
-                </li>
-              </ul>
-            </div>
-            <br>
-          </li>
-          <div class="card-footer d-flex justify-content-end my-1">
-            <button class="next-step"> Previous</button>
-            <button class="next-step">Next ></button>
+              </li>
+              <li class="list-group-item">How Much?</li>
+              <li class="list-group-item">
+                <label>
+                  <select id="cheese-amount" name="cheese">
+                    <option value="light">Light</option>
+                    <option value="normal">Normal</option>
+                    <option value="extra">Extra</option>
+                    <option value="double">Double</option>
+                  </select>
+                </label>
+              </li>
+            </ul>
           </div>
-        </ul>
-      </div>
+          <br>
+        </li>
+        <div class="card-footer d-flex justify-content-end my-1">
+          <button class="next-step"> Previous</button>
+          <button class="next-step">Next ></button>
+        </div>
+      </ul>
     </div>
-    <!-- Meats-->
-    <div class="tab-pane" id="pills-meat" role="tabpanel" aria-labelledby="pills-meat-tab">
-      <div class="card" style="width: 100%;">
-        <ul class="list-group list-group">
-          <li class="list-group-item toppings">Choose Your Meat</li>
-          <li class="list-group-item">
-            <!--Meats-->
-            <div class="card card-radio my-20" style="width: 100%;">
-              <ul class="list-group list-group">
+  </div>
+  <!-- Meats-->
+  <div class="tab-pane" id="pills-meat" role="tabpanel" aria-labelledby="pills-meat-tab">
+    <div class="card" style="width: 100%;">
+      <ul class="list-group list-group">
+        <li class="list-group-item toppings">Choose Your Meat</li>
+        <li class="list-group-item">
+          <!--Meats-->
+          <div class="card card-radio my-20" style="width: 100%;">
+            <ul class="list-group list-group">
 
-                <!-- <li class="list-group-item">
+              <!-- <li class="list-group-item">
                   <label>
                     <input type="checkbox" name="meat-toppings" value="pepperoni">
                   </label>
@@ -273,33 +286,33 @@
                   BBQ Chicken
                 </li> -->
 
-                <li v-for="meat in meatToppings" v-bind:key="meat.name" class="list-group-item">
-                  <label>
-                    <input type="checkbox" name="meat-toppings" value={{meat.name}}>
-                  </label>
-                  {{ meat.name }}
-                </li>
+              <li v-for="meat in meatToppings" v-bind:key="meat.name" class="list-group-item">
+                <label>
+                  <input type="checkbox" name="meat-toppings" value={{meat.name}}>
+                </label>
+                {{ meat.name }}
+              </li>
 
-              </ul>
-            </div>
-            <div class="card-footer d-flex justify-content-end my-1">
-              <button class="next-step"> Previous</button>
-              <button class="next-step">Next ></button>
-            </div>
-          </li>
-        </ul>
-      </div>
+            </ul>
+          </div>
+          <div class="card-footer d-flex justify-content-end my-1">
+            <button class="next-step"> Previous</button>
+            <button class="next-step">Next ></button>
+          </div>
+        </li>
+      </ul>
     </div>
-    <div class="tab-pane" id="pills-veggies" role="tabpanel" aria-labelledby="pills-veggies-tab">
-      <div class="card" style="width: 100%;">
-        <ul class="list-group list-group">
-          <li class="list-group-item toppings">Choose Veggies</li>
-          <li class="list-group-item">
-            <!--Non-Meats-->
-            <div class="card card-radio" style="width: 100%;">
-              <ul class="list-group list-group">
-                
-                <!-- <li class="list-group-item">
+  </div>
+  <div class="tab-pane" id="pills-veggies" role="tabpanel" aria-labelledby="pills-veggies-tab">
+    <div class="card" style="width: 100%;">
+      <ul class="list-group list-group">
+        <li class="list-group-item toppings">Choose Veggies</li>
+        <li class="list-group-item">
+          <!--Non-Meats-->
+          <div class="card card-radio" style="width: 100%;">
+            <ul class="list-group list-group">
+
+              <!-- <li class="list-group-item">
                   <label>
                     <input type="checkbox" name="non-meat-toppings" value="green-peppers">
                   </label>
@@ -354,30 +367,30 @@
                   Pineapple
                 </li> -->
 
-                <li v-for="veggie in veggieToppings" v-bind:key="veggie.name" class="list-group-item">
-                  <label>
-                    <input type="checkbox" name="non-meat-toppings" value={{veggie.name}}>
-                  </label>
-                  {{ veggie.name }}
-                </li>
+              <li v-for="veggie in veggieToppings" v-bind:key="veggie.name" class="list-group-item">
+                <label>
+                  <input type="checkbox" name="non-meat-toppings" value={{veggie.name}}>
+                </label>
+                {{ veggie.name }}
+              </li>
 
-              </ul>
-            </div>
-          </li>
-          <li class="list-group-item">
-          </li>
-        </ul>
-        <div class="card-footer d-flex justify-content-end">
-          <button class="next-step">Checkout
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em"
-              viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-              <path
-                d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-            </svg>
-          </button>
-        </div>
+            </ul>
+          </div>
+        </li>
+        <li class="list-group-item">
+        </li>
+      </ul>
+      <div class="card-footer d-flex justify-content-end">
+        <button class="next-step">Checkout
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+            viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+          </svg>
+        </button>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -393,9 +406,9 @@ export default {
       meatToppings: [],
       veggieToppings: [],
       cheeseToppings: [],
-      crustTypes:[],
-      sauces:[],
-      crustSizes:[],
+      crustTypes: [],
+      sauces: [],
+      crustSizes: [],
     }
   },
   created() {
@@ -421,23 +434,23 @@ export default {
       (response) => {
         this.cheeseToppings = response.data;
       });
-      ToppingsService.getMeat().then(
+    ToppingsService.getMeat().then(
       (response) => {
         this.meatToppings = response.data;
       });
-      ToppingsService.getSauce().then(
+    ToppingsService.getSauce().then(
       (response) => {
         this.sauces = response.data;
       });
-      ToppingsService.getCrust().then(
+    ToppingsService.getCrust().then(
       (response) => {
         this.crustTypes = response.data;
       });
-      ToppingsService.getVeggie().then(
+    ToppingsService.getVeggie().then(
       (response) => {
         this.veggieToppings = response.data;
       });
-      ToppingsService.getSize().then(
+    ToppingsService.getSize().then(
       (response) => {
         this.crustSizes = response.data;
       });
