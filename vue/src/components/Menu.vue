0b95e3dffe48126c1e01b099358a8529f403b441
Lauren Gates <mononoke813@gmail.com>
2024-06-04 11:19:16 -0500

    added images and menu

---
+++ vue/src/components/Menu.vue
@@ -0,0 +1,181 @@
+<template>
+    <section class="container-fluid">
+        <div class="row">
+            <div class="col-2">
+                <div class="menu">
+                    <div class="menu-items">
+                        <div class="menu-item">
+                            <h3>Pizzas</h3>
+                        </div>
+                        <div class="menu-item">
+                            <h3>Salads</h3>
+                        </div>
+                        <div class="menu-item">
+                            <h3>Sides</h3>
+                        </div>
+                        <div class="menu-item">
+                            <h3>Kid's Meal</h3>
+                        </div>
+                        <div class="menu-item">
+                            <h3>Beverages</h3>
+                        </div>
+                        <div class="menu-item">
+                            <h3>Desserts</h3>
+                        </div>
+                        <div class="menu-item">
+                            <h3>Deals</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-10">
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+                            aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
+                            aria-label="Slide 5"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5"
+                            aria-label="Slide 6"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6"
+                            aria-label="Slide 7"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../images/BBQ chicken pizza.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Back-End</h5>
+                                <p>BBQ sauce, BBQ chicken, onions, and tons of cheese</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/tomato basil pizza.jpeg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Front-End</h5>
+                                <p>Sliced tomatoes, mozzarella cheese, garlic, fresh basil, and extra-virgin olive oil</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/veggie pizza.webp" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Debugger</h5>
+                                <p>Pesto sauce with shredded mozzarella cheese and topped with fresh green peppers, black
+                                    olives,
+                                    onions, mushrooms, and fresh basil leaves.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/Meat Lovers.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Framework</h5>
+                                <p>Served with crumbled Italian sausage, bacon, and pepperoni.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/supreme pizza.webp" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Full-Stack</h5>
+                                <p>Delicious pepperoni, sausage, onions, green peppers, mushrooms, and olives.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/classic cheese pizza.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Constructor</h5>
+                                <p>A classic cheese pizza. Add some jalapenos to spicy up your night!</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../images/custom pizza.jpeg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The Polymorph</h5>
+                                <p>Design your pizza YOUR way!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<script></script>
+
+
+<style scoped>
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    background-color: #2892C4;
+}
+
+.menu-items {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+.menu-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70px;
+    width: 100%;
+    background-color: #F2DC9C;
+    border: 2px solid #2892C4;
+    border-radius: 5px;
+    color: #A4200B;
+}
+
+img {
+    height: 500px;
+    /* width: 100%; */
+    object-fit: cover;
+}
+
+.carousel-control-prev {
+    width: 5%;
+    height: 100%;
+    background-color: #A4200B;
+    border: 2px solid #A4200B;
+    border-radius: 5px;
+}
+
+.carousel-control-next {
+    width: 5%;
+    height: 100%;
+    background-color: #A4200B;
+    border: 2px solid #A4200B;
+    border-radius: 5px;
+}
+
+h5,
+p {
+    color: #F2DC9C;
+}
+
+.carousel-caption {
+    background-color: #A4200B;
+    border: 2px solid #A4200B;
+    border-radius: 5px;
+    opacity: .8;
+}
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <NavBar />
   <div class="home">
     <h1>Home</h1>
+    <Menu />
     <!-- <p>You are authenticated if you see this.</p> -->
   </div>
   <CustomPizzaMenu />
@@ -11,13 +12,15 @@
 
   import NavBar from '../components/NavBar.vue';
   import CustomPizzaMenu from '../components/CustomPizzaMenu.vue';
+  import Menu from '../components/Menu.vue';
 
   
 
 export default {
   components: {
     NavBar,
-    CustomPizzaMenu
+    CustomPizzaMenu,
+    Menu
   }
 }
 
