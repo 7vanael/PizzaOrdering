a40421f8240a2f1effc402fe96223e4a1deb9be8
cataclysmicserendipity <jenniferdgaylord@gmail.com>
2024-06-06 15:01:49 -0500

    get customer order

--- java/src/main/java/com/techelevator/dao/JdbcOrderDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcOrderDao.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class JdbcOrderDao implements OrderDao{
     private JdbcTemplate jdbcTemplate;
@@ -46,44 +49,69 @@ public class JdbcOrderDao implements OrderDao{
         try {
             SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
             Order order = new Order();
+            order.setId(id);
             Customer customer = new Customer();
             Item item = new Item();
             Pizza pizza = new Pizza();
+            List<Topping> toppings = new ArrayList<>();
+            List<Item> items = new ArrayList<>();
             int itemId = 0;
             int uniqueItemId = 0;
+            int customerId = 0;
 
             if (results.next()){
                 order.setTotal(results.getDouble("order_total"));
                 order.setStatus(results.getString("order_status"));
                 order.setType(results.getString("order_type"));
                 customer.setId(results.getInt("customer_id"));
-                order.setCustomer(customer);
+                customerId = results.getInt("customer_id");
 
                 String sql2 = "SELECT unique_item_id, item_id FROM order_items WHERE order_id = ?";
                 SqlRowSet results2 = jdbcTemplate.queryForRowSet(sql2, id);
                 while(results2.next()){
                     itemId = results2.getInt("item_id");
                     uniqueItemId = results2.getInt("unique_item_id");
-                    item.setPizza(pizza);
                     pizza.setId(results2.getInt("item_id"));
 
                 }
                 String sql3 = "SELECT pizza_name, pizza_size, crust_type, sauce_type FROM specialty_pizzas WHERE pizza_id = ?";
                 SqlRowSet results3 = jdbcTemplate.queryForRowSet(sql3, itemId);
-                while(results3.next()) {
-                    pizza.setName(results.getString("pizza_name"));
-                    pizza.setSize(results.getString("default" + "pizza_size"));
-                    pizza.setCrust(results.getString("default" + "crust_type"));
-                    pizza.setSauce(results.getString("default" + "sauce_type"));
+                if(results3.next()) {
+                    pizza.setName(results3.getString("pizza_name"));
+                    pizza.setSize("default " + results3.getString("pizza_size"));
+                    pizza.setCrust("default " + results3.getString("crust_type"));
+                    pizza.setSauce("default " + results3.getString("sauce_type"));
                 }
-                String sql4 = "SELECT topping FROM specialty_toppings WHERE "
+
+                String sql4 = "SELECT item_option, t.topping_type FROM order_item_options INNER JOIN toppings AS t ON t.topping = item_option WHERE item_id = ?";
+                SqlRowSet results4 = jdbcTemplate.queryForRowSet(sql4, uniqueItemId);
+                while(results4.next()){
+                    Topping topping = new Topping();
+                    topping.setName(results4.getString("item_option"));
+                    topping.setType(results4.getString("topping_type"));
+                    toppings.add(topping);
+                }
+                String sql5 = "SELECT customer_name, customer_email, customer_street_address, customer_city, customer_state, customer_zip FROM customers WHERE customer_id = ?";
+                SqlRowSet results5 = jdbcTemplate.queryForRowSet(sql5, customerId);
+                if(results5.next()){
+                    customer.setName(results5.getString("customer_name"));
+                    customer.setEmail(results5.getString("customer_email"));
+                    customer.setAddress(results5.getString("customer_street_address"));
+                    customer.setCity(results5.getString("customer_city"));
+                    customer.setState(results5.getString("customer_state"));
+                    customer.setZip(results5.getString("customer_zip"));
+                }
+                pizza.setToppings(toppings);
+                item.setPizza(pizza);
+                items.add(item);
+                order.setItems(items);
+                order.setCustomer(customer);
+
             }
+            return order;
 
         }catch(Exception e) {
             throw new DaoException(e.getMessage(), e);
         }
-
-
-        return null;
     }
 }
