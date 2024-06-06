9861d55261b3088361d6ded2e1ac44e6f952cf59
Lauren Gates <mononoke813@gmail.com>
2024-06-06 13:43:28 -0500

    fixing up menu.vue

--- vue/src/components/Menu.vue
+++ vue/src/components/Menu.vue
@@ -5,100 +5,65 @@
                 <div class="menu">
                     <div class="menu-items">
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Pizzas</h3></a>
+                            <a href="#pizzas">
+                                <h3>Pizzas</h3>
+                            </a>
                         </div>
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Salads</h3></a>
+                            <a href="#pizzas">
+                                <h3>Salads</h3>
+                            </a>
                         </div>
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Sides</h3></a>
+                            <a href="#pizzas">
+                                <h3>Sides</h3>
+                            </a>
                         </div>
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Kid's Meal</h3></a>
+                            <a href="#pizzas">
+                                <h3>Kid's Meal</h3>
+                            </a>
                         </div>
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Beverages</h3></a>
+                            <a href="#pizzas">
+                                <h3>Beverages</h3>
+                            </a>
                         </div>
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Desserts</h3></a>
+                            <a href="#pizzas">
+                                <h3>Desserts</h3>
+                            </a>
                         </div>
                         <div class="menu-item">
-                            <a href="#pizzas"><h3>Deals</h3></a>
+                            <a href="#pizzas">
+                                <h3>Deals</h3>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-10">
                 <div id="carouselExampleCaptions" class="carousel slide">
                     <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                            aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
-                            aria-label="Slide 4"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
-                            aria-label="Slide 5"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5"
-                            aria-label="Slide 6"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6"
-                            aria-label="Slide 7"></button>
+                        <button v-for="(pizza, index) in pizzas" :key="index" type="button"
+                            :data-bs-target="'#carouselExampleCaptions'" :data-bs-slide-to="index"
+                            :class="{ active: index === 0 }" :aria-current="index === 0 ? 'true' : undefined"
+                            :aria-label="'Slide ' + (index + 1)"></button>
                     </div>
+
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="../images/BBQ chicken pizza.jpg" class="d-block w-100" alt="...">
+                        <div v-for="(pizza, index) in pizzas" :key="pizza.name"
+                            :class="['carousel-item', { active: index === 0 }]">
+                    <img :src="images.image" class="d-block w-100" alt="pizza image">
+                            <!-- <img src="../images/BBQ chicken pizza.jpg" class="d-block w-100" alt="..."> -->
                             <div class="carousel-caption d-none d-md-block">
-                                <h5>The Back-End</h5>
-                                <p>BBQ sauce, BBQ chicken, onions, and tons of cheese</p>
+                                <h5>{{ pizza.name }}</h5>
+                                <p>{{ pizza.description }}</p>
                             </div>
+
                         </div>
-                        <div class="carousel-item">
-                            <img src="../images/tomato basil pizza.jpeg" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>The Front-End</h5>
-                                <p>Sliced tomatoes, mozzarella cheese, garlic, fresh basil, and extra-virgin olive oil</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../images/veggie pizza.webp" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>The Debugger</h5>
-                                <p>Pesto sauce with shredded mozzarella cheese and topped with fresh green peppers, black
-                                    olives,
-                                    onions, mushrooms, and fresh basil leaves.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../images/Meat Lovers.jpg" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>The Framework</h5>
-                                <p>Served with crumbled Italian sausage, bacon, and pepperoni.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../images/supreme pizza.webp" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>The Full-Stack</h5>
-                                <p>Delicious pepperoni, sausage, onions, green peppers, mushrooms, and olives.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../images/classic cheese pizza.jpg" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>The Constructor</h5>
-                                <p>A classic cheese pizza. Add some jalapenos to spicy up your night!</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../images/custom pizza.jpeg" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>The Polymorph</h5>
-                                <p>Design your pizza YOUR way!</p>
-                            </div>
-                        </div>
+
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
                         data-bs-slide="prev">
@@ -110,19 +75,69 @@
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
-                </div>
 
+                </div>
             </div>
-            <CustomPizzaMenu />
-            
+
         </div>
-        
+        <CustomPizzaMenu />
+
+
     </section>
 </template>
 
 
 <script>
+import ToppingsService from '../services/ToppingsService';
 import CustomPizzaMenu from './CustomPizzaMenu.vue';
+
+export default {
+    data() {
+        return {
+            images: [
+                { 
+                    name: 'AI margherita pizza',
+                    image: '../images/AI margherita pizza.webp'
+                },
+                { 
+                    name: 'BBQ chicken pizza',
+                    image: '../images/BBQ chicken pizza.jpg'
+                },
+                { 
+                    name: 'Classic Cheese',
+                    image: '../images/classic cheese pizza.jpg'
+                },
+                { 
+                    name: 'Custom Pizza',
+                    image: '../images/custom pizza.jpg'
+                },
+                { 
+                    name: 'Meat Lovers',
+                    image: '../images/Meat Lovers.jpg'
+                },
+                { 
+                    name: 'Supreme pizza',
+                    image: '../images/supreme pizza.webp'
+                },
+                { 
+                    name: 'Tomato Basil pizza',
+                    image: '../images/tomato basil pizza.jpeg'
+                },
+                { 
+                    name: 'Veggie Pizza',
+                    image: '../images/veggie pizza.webp'
+                }
+                
+        ],
+        pizzas: [],
+        };
+    },
+    created() {
+        ToppingsService.getPizzas().then(response => {
+            this.pizzas = response.data;
+        });
+    }
+}
 
 
 
@@ -138,6 +153,7 @@ import CustomPizzaMenu from './CustomPizzaMenu.vue';
     padding: 20px;
     min-height: 40dvh;
 }
+
 .row {
     display: flex;
     min-height: 100%;
@@ -149,6 +165,7 @@ import CustomPizzaMenu from './CustomPizzaMenu.vue';
     flex-direction: column;
     height: 100%;
 }
+
 .menu-item {
     display: flex;
     justify-content: center;
