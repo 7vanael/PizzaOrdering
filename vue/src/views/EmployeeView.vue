015bcb276d176fe2709d709973f2dcce16eb0cbc
Zachary Evans <ze246109@gmail.com>
2024-06-07 19:01:52 -0400

    add custom menu to employee view

--- vue/src/views/EmployeeView.vue
+++ vue/src/views/EmployeeView.vue
@@ -13,7 +13,7 @@
                         <input type="text" id="name" name="name" v-model="newPizza.pizza_name">
                         <label for="description">Pizza description:</label>
                         <input type="text" id="description" name="description" v-model="newPizza.pizza_description">
-
+                        <CustomPizzaMenu/>
                         <!-- TODO: INSERT THE CUSTOM BUILD MENU HERE?? TO SELECT THE REST OF THE OPTIONS?? -->
                         <!-- Do we need to include an image file? Can we accept it to the images folder? -->
                         <!-- submit form to add new specialty pizza to DB -->
@@ -30,7 +30,6 @@
         </div>
 
     </div>
-
 </template>
 
 <script scoped>
@@ -39,22 +38,24 @@ import PizzaAvailability from '../components/PizzaAvailability.vue';
 import ToppingsAvailability from '../components/ToppingsAvailability.vue';
 import Navbar from '../components/Navbar.vue';
 import ToppingsService from '../services/ToppingsService';
+import CustomPizzaMenu from '../components/CustomPizzaMenu.vue';
 
-export default{
+export default {
     components: {
         Navbar,
-    PizzaAvailability,
-    ToppingsAvailability
-},
-    data(){
-        return{
+        PizzaAvailability,
+        ToppingsAvailability,
+        CustomPizzaMenu
+    },
+    data() {
+        return {
             showPizzaForm: false,
             newPizza: {
                 pizza_name: '',
-                pizza_size: '', 
-                crust_type: '', 
-                sauce_type: '', 
-                pizza_available: true, 
+                pizza_size: '',
+                crust_type: '',
+                sauce_type: '',
+                pizza_available: true,
                 pizza_description: ''
             }
         }
@@ -62,20 +63,20 @@ export default{
     methods: {
         togglePizzaForm() {
             this.showPizzaForm = !this.showPizzaForm;
-        },  
-        addNewPizza (){
+        },
+        addNewPizza() {
             // Fabulously slick code that will send the newPizza on over to the DB
             this.clearForm();
         },
-        clearForm(){
-                this.newPizza.pizza_name= '';
-                this.newPizza.pizza_size= ''; 
-                this.newPizza.crust_type= ''; 
-                this.newPizza.sauce_type= '';
-                this.newPizza.pizza_available= true; 
-                this.newPizza.pizza_description= '';
-            
-        }   
+        clearForm() {
+            this.newPizza.pizza_name = '';
+            this.newPizza.pizza_size = '';
+            this.newPizza.crust_type = '';
+            this.newPizza.sauce_type = '';
+            this.newPizza.pizza_available = true;
+            this.newPizza.pizza_description = '';
+
+        }
     }
 
 }
@@ -86,9 +87,9 @@ export default{
 
 <style>
 .container {
-  background-color: #F2DC9C;
-  border: 2px solid #2892C4;
-  border-radius: 5px;
-  padding: 20px;
+    background-color: #F2DC9C;
+    border: 2px solid #2892C4;
+    border-radius: 5px;
+    padding: 20px;
 }
 </style>
