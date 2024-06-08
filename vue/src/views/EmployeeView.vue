e5b58303e820cbbe4aa368d40bbffd353c83f8a3
Lauren Gates <mononoke813@gmail.com>
2024-06-08 16:54:02 -0500

    employeeview updates

--- vue/src/views/EmployeeView.vue
+++ vue/src/views/EmployeeView.vue
@@ -1,54 +1,58 @@
 <template>
     <Navbar />
 
-    <div class="container">
-        <!-- Header -->
-        <h1> Welcome, Syntax Slices Employee!</h1>
-        <h2> Here you can create a new pizza to add to the menu and adjust toppings availability.</h2>
-        <!-- Create New Pizza-->
-        <div>
-            <button class="toggle" @click="toggleToppingForm">Add Specialty Pizza</button>
-            <button class="toggle" @click="toggleToppingForm">Toppings Availability Menu</button>
-            <!-- <div class="row">
-                <CustomPizzaMenu :showButton="false" />
-            </div> -->
-            <!-- <div class="row">
-                <div class="col-5">
+    <div>
+        <div class="container">
+            <h1> Welcome, Syntax Slices Employee!</h1>
+            <h2> Here you can create a new pizza to add to the menu and adjust toppings availability.</h2>
+            <!-- <button class="toggle" @click="toggleToppingForm">Toppings Availability Menu</button> -->
+            <hr />
+
+            <div class="row">
+                <button class="toggle" @click="toggleComponent">{{ buttonText }}</button>
+            </div>
+            <div class="row">
+                <CustomPizzaMenu v-if="showCustomPizzaMenu" :showButton="false" />
+                <ToppingsAvailability v-else />
+            </div>
+            <div class="row" v-if="showCustomPizzaMenu">
+                <div class="col-4">
                     <label for="name">Name Your Creation:
                         <input type="text" id="name" name="name" v-model="newPizza.pizza_name">
                     </label>
                 </div>
-                <div class="col-5">
+                <div class="col-4">
                     <label for="description">Pizza description:
                         <input type="text" id="description" name="description" v-model="newPizza.pizza_description">
                     </label>
                 </div>
-                 TODO: INSERT THE CUSTOM BUILD MENU HERE?? TO SELECT THE REST OF THE OPTIONS?? -->
+                <!-- TODO: INSERT THE CUSTOM BUILD MENU HERE?? TO SELECT THE REST OF THE OPTIONS??  -->
                 <!-- Do we need to include an image file? Can we accept it to the images folder? -->
                 <!-- submit form to add new specialty pizza to DB -->
-                <!-- <div class="col-2">
+                <div class="col-3">
                     <button class="submitButton" type="submit">Add New Pizza to Menu</button>
+                </div>
 
-                </div> -->
-            <!-- </div> --> 
-        
-        <!-- Toppings Availability Menu  -->
-        <div class="row">
-            <ToppingsAvailability />
 
-        <!-- <PizzaAvailability /> -->
-        <!-- <button class="toggle" v-on:click="togglePizzaForm"> Add a Specialty Pizza</button>
+                <!-- Toppings Availability Menu  -->
+                <!-- <div class="row">
+                <ToppingsAvailability v-show="showToppingsAvailability" /> -->
+                <!-- <CustomPizzaMenu /> -->
+
+                <!-- <PizzaAvailability /> -->
+                <!-- <button class="toggle" v-on:click="togglePizzaForm"> Add a Specialty Pizza</button>
                     <form v-on:submit.prevent="addNewPizza" v-show="showPizzaForm">
-                    </form>
-        </div> -->
-        <!-- <div v-show="!isToppingForm"> -->
-        <!-- <div class="col"> -->
-                        <!-- <ToppingsAvailability /> -->
-                    <!-- </div> -->
-        <!-- <div class="col"> -->
-                         <!-- Pending orders  -- Should this be displaying another component? -->
+                    </form> -->
+
+                <!-- <div v-show="!isToppingForm"> -->
+                <!-- <div class="col"> -->
+                <!-- <ToppingsAvailability /> -->
+                <!-- </div> -->
+                <!-- <div class="col"> -->
+                <!-- Pending orders  -- Should this be displaying another component? -->
+            </div>
+
         </div>
-</div>
     </div>
 </template>
 
@@ -69,7 +73,8 @@ export default {
     },
     data() {
         return {
-            showPizzaForm: false,
+            showCustomPizzaMenu: true,
+            // showPizzaForm: false,
             newPizza: {
                 pizza_name: '',
                 pizza_size: '',
@@ -80,9 +85,14 @@ export default {
             }
         }
     },
+    computed: {
+        buttonText() {
+            return this.showCustomPizzaMenu ? 'Toppings Availability Menu' : 'Add Specialty Pizza';
+        }
+    },
     methods: {
-        toggleToppingForm() {
-            this.isToppingForm = !this.isToppingForm;
+        toggleComponent() {
+            this.showCustomPizzaMenu = !this.showCustomPizzaMenu;
         },
         addNewPizza() {
             // Fabulously slick code that will send the newPizza on over to the DB
@@ -106,10 +116,58 @@ export default {
 
 
 <style>
+h1,
+h2 {
+    text-align: center;
+    color: #A4200B;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+
+h1 {
+    font-weight: bold;
+}
+
 .container {
+    display: flex;
+    flex-direction: column;
     background-color: #F2DC9C;
     border: 2px solid #2892C4;
     border-radius: 5px;
-    padding: 20px;
+    padding-bottom: none;
+}
+
+.row {
+    display: flex;
+    justify-content: center;
+}
+
+button.toggle {
+    background-color: #2892C4;
+    width: 50%;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 2em;
+    ;
+}
+label, input {
+    font-size: 16pt;
+    margin-top: 5px;
+    font-weight: bold;
+    color: #A4200B;
+    letter-spacing: 2px;
+}
+.submitButton {
+    background-color: #A4200B;
+    color: #F7C516;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 1.5em;
+    font-weight: bold;
+    margin-top: 10px;
+    height: 100%;
 }
 </style>
